0aa6e584359e3c12d4f12f8447813f0d1218e8e5
hv6kyh <hv6kyh@gmail.com>
2021-02-07 04:55:21 +0900

    에러 수정

--- community-server/src/app.module.ts
+++ community-server/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DoctorModule } from './doctor/doctor.module';
 import { ormConfig } from '../../domains';
 
 @Module({
-  imports: [DoctorModule, TypeOrmModule.forRoot(ormConfig as any)],
+  imports: [TypeOrmModule.forRoot(ormConfig as any)],
   controllers: [AppController],
   providers: [AppService],
 })
--- diagnosis-server/src/app.module.ts
+++ diagnosis-server/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { ormConfig } from '../../domains';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(ormConfig as any)],
+  imports: [TypeOrmModule.forRoot(ormConfig as any)],
   controllers: [AppController],
   providers: [AppService],
 })
